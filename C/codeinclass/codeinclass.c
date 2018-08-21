0b76d34779fe9e983c59022f3a55e8f1c6f8631e
caglisca <cg.academyp@gmail.com>
2018-08-21 16:46:46 -0400

    C exercitii

---
+++ C/codeinclass/codeinclass.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	int c = 10;
+	while (c > 0) {
+		puts("I must not write code in class.");
+		c = c - 1;
+	}
+}
---
+++ C/exbf/exbf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() 
+{
+	char ex[20];
+	puts ("Enter girlfriend's name: ");
+	scanf ("%19s", ex);
+	printf ("Dear %s.\n\n\tYou're history.\n", ex);
+}
+
